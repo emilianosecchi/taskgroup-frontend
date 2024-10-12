465b1426683caca2940bd7a4255c450e291191e9
Emiliano Secchi <emilianosecchi012@gmail.com>
2024-10-11 21:22:33 -0300

    Fetch de las notificaciones al iniciar sesión

--- src/api/Notification.js
+++ src/api/Notification.js
@@ -1,11 +1,29 @@
-export function getAllNotificationsForUser(userId) {
+import { getAxiosInstance, getFullUrl } from "../api/AxiosConfiguration";
 
+const axios = getAxiosInstance();
+
+const resourceName = "/notification";
+
+export async function fetchAllNotificationsForUser(userId) {
+  try {
+    const response = await axios.get(
+      getFullUrl(resourceName, "user/" + userId),
+      { withCredentials: true }
+    );
+    console.log(response.data);
+    return {
+      success: true,
+      notifications: response.data,
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      success: false,
+      error: e,
+    };
+  }
 }
 
-export function markNotificationAsRead(notificationId) {
+export async function markNotificationAsRead(notificationId) {}
 
-}
-
-export function markAllNotificationsAsRead(userId) {
-    
-}
+export async function markAllNotificationsAsRead(userId) {}
--- src/context/NotificationContext.js
+++ src/context/NotificationContext.js
@@ -2,6 +2,11 @@ import { useEffect, useState, createContext } from "react";
 import SockJS from "sockjs-client";
 import Stomp from "stompjs";
 import { useAuth } from "../hooks/useAuth";
+import {
+  fetchAllNotificationsForUser,
+  markAllNotificationsAsRead as apiMarkAllNotificationsAsRead,
+  markNotificationAsRead as apiMarkNotificationAsRead,
+} from "../api/Notification";
 
 export const NotificationContext = createContext();
 
@@ -11,9 +16,24 @@ export function NotificationContextProvider({ children }) {
   const { getUserId, getJwtToken } = useAuth();
 
   useEffect(() => {
+    async function fetchNotifications(userId) {
+      const response = await fetchAllNotificationsForUser(userId);
+      if (response.success) {
+        setNotifications(response.notifications);
+        for (const n of response.notifications) {
+          if (!n.isRead) {
+            setHasNotification(true);
+            //playNotificationSound();
+            break;
+          }
+        }
+      }
+    }
     const userId = getUserId();
     const jwtToken = getJwtToken();
-    const websocketUrl = "http://localhost:8080/ws-notifications";
+    fetchNotifications(userId);
+    const websocketUrl =
+      process.env.REACT_APP_BASE_BACKEND_API_URL + "/ws-notifications";
     const socket = new SockJS(websocketUrl);
     const stompClient = Stomp.over(socket);
     const connectionHeaders = {
@@ -22,7 +42,6 @@ export function NotificationContextProvider({ children }) {
     stompClient.connect(
       connectionHeaders,
       () => {
-        console.log("Conexión establecida");
         stompClient.subscribe(
           `/topic/user/${userId}/notifications`,
           (notification) => {
@@ -40,18 +59,27 @@ export function NotificationContextProvider({ children }) {
         console.log("Error de conexión con el WebSocket: " + error);
       }
     );
+
+    return () => {
+      if (stompClient && stompClient.connected) {
+        stompClient.disconnect(() => {
+          console.log("WebSocket desconectado");
+        });
+      }
+    };
   }, [getUserId, getJwtToken]);
 
   const playNotificationSound = () => {
-    const audio = new Audio('/notification.mp3');
+    const audio = new Audio("/notification.mp3");
     audio.play();
   };
 
-  const markNotificationAsRead = (idNotification) => {
-    // implementar
+  const markNotificationAsRead = (notificationId) => {
+    apiMarkNotificationAsRead(notificationId);
   };
 
   const markAllNotificationsAsRead = () => {
+    apiMarkAllNotificationsAsRead(getUserId());
     setHasNotification(false);
   };
 
